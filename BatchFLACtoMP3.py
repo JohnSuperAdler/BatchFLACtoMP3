078acb4b65ccfdd8b9a9c8f1ec1403b062545cf4
Phidias Chiang <phidias@dt42ai.com>
2022-08-10 21:15:34 +0800

    refactor: modularize conversion logic

--- BatchFLACtoMP3.py
+++ BatchFLACtoMP3.py
@@ -71,66 +71,72 @@ def count_files():
 count_1 = count_files()
 
 ### Conversion
-for a, b, c in os.walk(path_src):
-    for src_fn in c:
-        count_2 += 1
-        ### Filename and path
-        src_fullpath = os.path.join(a, src_fn)
-        dst_fn       = os.path.splitext(src_fn)[0] + '.mp3'
-        dst_rela_dir = os.path.relpath(os.path.dirname(src_fullpath), path_src)
-        dst_full_dir = os.path.join(path_dst, dst_rela_dir)
-        dst_fullpath = os.path.join(dst_full_dir, dst_fn)
-        if not os.path.exists(dst_full_dir):
-            os.makedirs(dst_full_dir)
-        ### Check src file type
-        read_file = File(src_fullpath)
-        file_type = type(read_file)
-        
-        ### File type manifold
-        if file_type == mutagen.flac.FLAC:
-            # TAG
-            new_tag_di = {}
-            for t in read_file.keys():
-                new_tag_di[t] = read_file[t][0]
-            # Convert
-            flac_audio = AudioSegment.from_file(src_fullpath, format='flac')
-            flac_audio.export(dst_fullpath, format=file_format, bitrate=bitrate, tags=new_tag_di)
-            # Art
-            try:
-                art = read_file.pictures[0].data
-                audio = MP3(dst_fullpath, ID3=ID3)    
-                audio.tags.add(APIC(encoding=0, # 3 is for utf-8
-                                    mime='image/png', # image/jpeg or image/png
-                                    type=3, # 3 is for the cover image
-                                    data=art))
-                audio.save()
-            except:
-                pass
-            conversion_count +=1
-        elif file_type == mutagen.wave.WAVE:
-            # Convert
-            wav_audio = AudioSegment.from_file(src_fullpath, format='wav')
-            wav_audio.export(dst_fullpath, format=file_format, bitrate=bitrate)
-            # Write tags/art
-            try:
-                for t in read_file.tags.keys():
-                    audio = MP3(dst_fullpath, ID3=ID3)
-                    audio.tags.add(read_file.tags[t])
+def conversion():
+    conversion_count = 0
+    count_2 = 0
+    for a, b, c in os.walk(path_src):
+        for src_fn in c:
+            count_2 += 1
+            ### Filename and path
+            src_fullpath = os.path.join(a, src_fn)
+            dst_fn       = os.path.splitext(src_fn)[0] + '.mp3'
+            dst_rela_dir = os.path.relpath(os.path.dirname(src_fullpath), path_src)
+            dst_full_dir = os.path.join(path_dst, dst_rela_dir)
+            dst_fullpath = os.path.join(dst_full_dir, dst_fn)
+            if not os.path.exists(dst_full_dir):
+                os.makedirs(dst_full_dir)
+            ### Check src file type
+            read_file = File(src_fullpath)
+            file_type = type(read_file)
+            
+            ### File type manifold
+            if file_type == mutagen.flac.FLAC:
+                # TAG
+                new_tag_di = {}
+                for t in read_file.keys():
+                    new_tag_di[t] = read_file[t][0]
+                # Convert
+                flac_audio = AudioSegment.from_file(src_fullpath, format='flac')
+                flac_audio.export(dst_fullpath, format=file_format, bitrate=bitrate, tags=new_tag_di)
+                # Art
+                try:
+                    art = read_file.pictures[0].data
+                    audio = MP3(dst_fullpath, ID3=ID3)    
+                    audio.tags.add(APIC(encoding=0, # 3 is for utf-8
+                                        mime='image/png', # image/jpeg or image/png
+                                        type=3, # 3 is for the cover image
+                                        data=art))
                     audio.save()
-            except:
-                pass
-            conversion_count +=1
-        #elif (file_type == mutagen.mp3.MP3) or (file_type == mutagen.mp4.MP4) or (file_type == mutagen.asf.ASF):
-        elif file_type == mutagen.mp3.MP3:
-            # Copy file
-            shutil.copy(src_fullpath, dst_fullpath)
-            copy_count += 1
-        else:
-            print(f'File type "{file_type}" not in deal list. Filename: "{src_fullpath}"')
-            continue
-        if (count_2 % 100) == 0:
-            print(f'{count_2}/{count_1} files process done. {happy_time(loop_start, time.time())}')
+                except:
+                    pass
+                conversion_count +=1
+            elif file_type == mutagen.wave.WAVE:
+                # Convert
+                wav_audio = AudioSegment.from_file(src_fullpath, format='wav')
+                wav_audio.export(dst_fullpath, format=file_format, bitrate=bitrate)
+                # Write tags/art
+                try:
+                    for t in read_file.tags.keys():
+                        audio = MP3(dst_fullpath, ID3=ID3)
+                        audio.tags.add(read_file.tags[t])
+                        audio.save()
+                except:
+                    pass
+                conversion_count +=1
+            #elif (file_type == mutagen.mp3.MP3) or (file_type == mutagen.mp4.MP4) or (file_type == mutagen.asf.ASF):
+            elif file_type == mutagen.mp3.MP3:
+                # Copy file
+                shutil.copy(src_fullpath, dst_fullpath)
+                copy_count += 1
+            else:
+                print(f'File type "{file_type}" not in deal list. Filename: "{src_fullpath}"')
+                continue
+            if (count_2 % 100) == 0:
+                print(f'{count_2}/{count_1} files process done. {happy_time(loop_start, time.time())}')
 
+    return conversion_count
+
+conversion_count = conversion()
 
 ##### END UP
 
